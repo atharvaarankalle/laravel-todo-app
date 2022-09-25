3471bf1e127aba1265107d68a987ebbdacb8ca0a
atharvaarankalle <atharvanz@gmail.com>
2022-09-26 12:46:37 +1300

    feat: Add form input and button

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,17 @@
 import { createApp } from 'vue';
-
 import App from "./vue/app.vue";
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add( faPlusSquare, faTrash );
 
 const app = createApp({
     components: { App }
 })
 
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.mount("#app");
 
 
