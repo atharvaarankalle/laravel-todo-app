8b6c7855d80b6087c87777634e4bb4fcf7282705
atharvaarankalle <atharvanz@gmail.com>
2022-09-26 12:56:27 +1300

    feat: Linked database with list view

--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -4,17 +4,37 @@
             <h2 id="title">Todo List</h2>
             <add-item-form />
         </div>
-        <list-view />
+        <list-view :items="items" />
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 import addItemForm from './addItemForm';
 import listView from './listView';
 export default {
     components: {
         addItemForm,
         listView
+    },
+    data: function () {
+        return {
+            items: []
+        }
+    },
+    methods: {
+        getList() {
+            axios.get('api/items')
+            .then( response => {
+                this.items = response.data;
+            })
+            .catch( error => {
+                console.log(error);
+            })
+        }
+    },
+    created() {
+        this.getList();
     }
 
 }
