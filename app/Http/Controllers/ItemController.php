2e1ec672b798ce8b23897e8b259ba87f19f7f503
atharvaarankalle <atharvanz@gmail.com>
2022-09-25 23:17:28 +1300

    feat: Add update method

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Models\Item;
+use Illuminate\Support\Carbon;
 
 class ItemController extends Controller
 {
@@ -76,7 +76,17 @@ class ItemController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $existingItem = Item::find( $id );
+
+        if ( $existingItem ) {
+            $existingItem->completed = $request->item['completed'] ? true : false;
+            $existingItem->completed_at = $request->item['completed'] ? Carbon::now() : null;
+            $existingItem->save();
+            
+            return $existingItem;
+        }
+
+        return "Item not found!";
     }
 
     /**
